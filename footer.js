5687755ee4e60432b4749a5110ccf1c13c75feee
Pmasuchette <pmasuchette@gmail.com>
2025-10-05 19:10:11 -0300

    update all files

---
+++ footer.js
@@ -0,0 +1,11 @@
+  document.write(`
+   <footer class="site-footer">
+    <div class="contato">Contato:</div>
+    <div class="redes-sociais">
+      <div class="rede"><img src="rs1.png" alt="Twitter"><span>Twitter: @PequenosNavegantes</span></div>
+      <div class="rede"><img src="rs2.png" alt="Instagram"><span>Instagram: @PequenosNavegantes</span></div>
+      <div class="rede"><img src="rs3.png" alt="E-mail"><span>E-mail: contato@pnavegantes.com.br</span></div>
+    </div>
+    <div class="marcaregistrada">© 2025 Pequenos Navegantes. Todos os direitos reservados.</div>
+  </footer>
+`);
---
+++ header.js
@@ -0,0 +1,16 @@
+   document.write(`<header class="header-areas">
+  <!-- Logo clicável -->
+  <a href="index.html" class="logo">
+    <img src="logo.jpg" alt="Logo Pequenos Navegantes">
+  </a>
+
+  <!-- Botões das seis áreas -->
+  <nav class="areas-nav">
+    <a href="pe_na_areia.html">Pé na Areia</a>
+    <a href="beira_mar.html">Beira Mar</a>
+    <a href="corais.html">Corais</a>
+    <a href="alto_mar.html">Alto Mar</a>
+    <a href="profundezas.html">Profundezas</a>
+    <a href="polos.html">Polos</a>
+  </nav>
+</header>`);
