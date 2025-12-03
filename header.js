a13a6f4ab926686ec286587b43f17c9eb63b7b5e
Gab-cunha-silva <gacunhadasilva@gmail.com>
2025-12-03 00:51:24 -0300

    Update header.js

--- header.js
+++ header.js
@@ -6,15 +6,16 @@
 
 
   <nav class="areas-nav">
-    <a href="./html/pe_na_areia.html">Pé na Areia</a>
-    <a href="./html/beira_mar.html">Beira Mar</a>
-    <a href="./html/corais.html">Corais</a>
-    <a href="./html/alto_mar.html">Alto Mar</a>
-    <a href="./html/profundezas.html">Profundezas</a>
-    <a href="./html/polos.html">Polos</a>
+    <a href="./html/pe_na_areia.html" class="meu-botao-cabeçalho-areas pé-na-areia-botões">Pé na Areia</a>
+    <a href="./html/beira_mar.html" class="meu-botao-cabeçalho-areas beira-mar-botões">Beira Mar</a>
+    <a href="./html/corais.html" class="meu-botao-cabeçalho-areas corais-botões">Corais</a>
+    <a href="./html/alto_mar.html" class="meu-botao-cabeçalho-areas alto-mar-botões">Alto Mar</a>
+    <a href="./html/profundezas.html" class="meu-botao-cabeçalho-areas profundezas-botões">Profundezas</a>
+    <a href="./html/polos.html" class="meu-botao-cabeçalho-areas polos-botões">Polos</a>
   </nav>
 
 </header>`);
+
 
 
 
