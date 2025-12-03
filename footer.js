fe7ec77df4d14a8f20b9ed6dd88009c2225af83a
Gab-cunha-silva <gacunhadasilva@gmail.com>
2025-12-03 00:22:53 -0300

    Update footer.js

--- footer.js
+++ footer.js
@@ -2,11 +2,12 @@
    <footer class="site-footer">
     <div class="contato">Contato:</div>
     <div class="redes-sociais">
-      <div class="rede"><img src="./fotos/rs1.png" alt="Twitter"><span>Twitter: @PequenosNavegantes</span></div>
-      <div class="rede"><img src="./fotos/rs2.png" alt="Instagram"><span>Instagram: @PequenosNavegantes</span></div>
-      <div class="rede"><img src="./fotos/rs3.png" alt="E-mail"><span>E-mail: contato@pnavegantes.com.br</span></div>
+      <div class="rede"><img src="fotos/rs1.png" alt="Twitter"><span>Twitter: @PequenosNavegantes</span></div>
+      <div class="rede"><img src="fotos/rs2.png" alt="Instagram"><span>Instagram: @PequenosNavegantes</span></div>
+      <div class="rede"><img src="fotos/rs3.png" alt="E-mail"><span>E-mail: contato@pnavegantes.com.br</span></div>
     </div>
     <div class="marcaregistrada">© 2025 Pequenos Navegantes. Todos os direitos reservados.</div>
   </footer>
 `);
+
 
