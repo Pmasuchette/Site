8251351dba3038e33bd933c21a936dd4f0f853bd
RafaelDellis <Rafael.dellis@aluno.ifsp.edu.br>
2025-12-04 23:16:33 -0300

    Add files via upload

--- footer.js
+++ footer.js
@@ -1,14 +1,18 @@
-  document.write(`
-   <footer class="site-footer">
+// footer.js
+document.addEventListener("DOMContentLoaded", function() {
+  const footerHTML = `
+  <footer class="site-footer">
     <div class="contato">Contato:</div>
     <div class="redes-sociais">
-      <div class="rede"><img src="./fotos/rs1.png" alt="Twitter"><span>Twitter: @PequenosNavegantes</span></div>
-      <div class="rede"><img src="./fotos/rs2.png" alt="Instagram"><span>Instagram: @PequenosNavegantes</span></div>
-      <div class="rede"><img src="./fotos/rs3.png" alt="E-mail"><span>E-mail: contato@pnavegantes.com.br</span></div>
+      <div class="rede"><img src="../fotos/X.png"><span>X: @PequenosNavegantes</span></div>
+      <div class="rede"><img src="../fotos/instagram.png"><span>Instagram: @PequenosNavegantes</span></div>
+      <div class="rede"><img src="../fotos/gmail.webp"><span>E-mail: contato@pnavegantes.com.br</span></div>
     </div>
     <div class="marcaregistrada">© 2025 Pequenos Navegantes. Todos os direitos reservados.</div>
   </footer>
-`);
+  `;
+  document.body.insertAdjacentHTML('beforeend', footerHTML);
+});
 
 
 
--- header.js
+++ header.js
@@ -1,17 +1,17 @@
    document.write(`
    <header class="header-areas">
-  <a href="index.html" class="logo">
-    <img src="./fotos/logo.png" alt="Logo Pequenos Navegantes">
+  <a href="../index.html" class="logo">
+    <img src="../fotos/logo.png" alt="Logo Pequenos Navegantes">
   </a>
 
 
   <nav class="areas-nav">
-    <a href="./html/pe_na_areia.html" class="meu-botao-cabeçalho-areas pé-na-areia-botões">Pé na Areia</a>
-    <a href="./html/beira_mar.html" class="meu-botao-cabeçalho-areas beira-mar-botões">Beira Mar</a>
-    <a href="./html/corais.html" class="meu-botao-cabeçalho-areas corais-botões">Corais</a>
-    <a href="./html/alto_mar.html" class="meu-botao-cabeçalho-areas alto-mar-botões">Alto Mar</a>
-    <a href="./html/profundezas.html" class="meu-botao-cabeçalho-areas profundezas-botões">Profundezas</a>
-    <a href="./html/polos.html" class="meu-botao-cabeçalho-areas polos-botões">Polos</a>
+    <a href="../html/pe_na_areia.html" class="meu-botao-cabeçalho-areas pé-na-areia-botões">Pé na Areia</a>
+    <a href="../html/beira_mar.html" class="meu-botao-cabeçalho-areas beira-mar-botões">Beira Mar</a>
+    <a href="../html/corais.html" class="meu-botao-cabeçalho-areas corais-botões">Corais</a>
+    <a href="../html/alto_mar.html" class="meu-botao-cabeçalho-areas alto-mar-botões">Alto Mar</a>
+    <a href="../html/profundezas.html" class="meu-botao-cabeçalho-areas profundezas-botões">Profundezas</a>
+    <a href="../html/polos.html" class="meu-botao-cabeçalho-areas polos-botões">Polos</a>
   </nav>
 
 </header>`);
